1c198ed8bf4f7adffbb584da513732e49e39c4a7
Divyarajsinhsolanki <solanki.divyarajsinhp@gmail.com>
2022-06-24 17:43:02 +0530

    first

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+ 
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+    resource :user, only: [:show, :update]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
